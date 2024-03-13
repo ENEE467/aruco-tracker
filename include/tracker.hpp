1396ff047bb0f7347bea58b2f0a6ffb8cef9d6ac
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-03-13 15:59:51 -0400

    For now: Retaining the boilerplate write function for reference

--- include/tracker.hpp
+++ include/tracker.hpp
@@ -117,34 +117,34 @@ void readConfigFile(const std::string& filename, calibrationOptions& options);
 void writeConfigFile();
 inline static bool saveCameraParams(const std::string &filename, cv::Size imageSize, float aspectRatio, int flags,
                                     const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs, double totalAvgErr) {
-    cv::FileStorage fs(filename, cv::FileStorage::WRITE);
-    if (!fs.isOpened())
-        return false;
+  cv::FileStorage fs(filename, cv::FileStorage::WRITE);
+  if (!fs.isOpened())
+    return false;
 
-    time_t tt;
-    time(&tt);
-    struct tm *t2 = localtime(&tt);
-    char buf[1024];
-    strftime(buf, sizeof(buf) - 1, "%c", t2);
+  time_t tt;
+  time(&tt);
+  struct tm *t2 = localtime(&tt);
+  char buf[1024];
+  strftime(buf, sizeof(buf) - 1, "%c", t2);
 
-    fs << "calibration_time" << buf;
-    fs << "image_width" << imageSize.width;
-    fs << "image_height" << imageSize.height;
+  fs << "calibration_time" << buf;
+  fs << "image_width" << imageSize.width;
+  fs << "image_height" << imageSize.height;
 
-    if (flags & cv::CALIB_FIX_ASPECT_RATIO) fs << "aspectRatio" << aspectRatio;
+  if (flags & cv::CALIB_FIX_ASPECT_RATIO) fs << "aspectRatio" << aspectRatio;
 
-    if (flags != 0) {
-        sprintf(buf, "flags: %s%s%s%s",
-                flags & cv::CALIB_USE_INTRINSIC_GUESS ? "+use_intrinsic_guess" : "",
-                flags & cv::CALIB_FIX_ASPECT_RATIO ? "+fix_aspectRatio" : "",
-                flags & cv::CALIB_FIX_PRINCIPAL_POINT ? "+fix_principal_point" : "",
-                flags & cv::CALIB_ZERO_TANGENT_DIST ? "+zero_tangent_dist" : "");
-    }
-    fs << "flags" << flags;
-    fs << "camera_matrix" << cameraMatrix;
-    fs << "distortion_coefficients" << distCoeffs;
-    fs << "avg_reprojection_error" << totalAvgErr;
-    return true;
+  if (flags != 0) {
+    sprintf(buf, "flags: %s%s%s%s",
+      flags & cv::CALIB_USE_INTRINSIC_GUESS ? "+use_intrinsic_guess" : "",
+      flags & cv::CALIB_FIX_ASPECT_RATIO ? "+fix_aspectRatio" : "",
+      flags & cv::CALIB_FIX_PRINCIPAL_POINT ? "+fix_principal_point" : "",
+      flags & cv::CALIB_ZERO_TANGENT_DIST ? "+zero_tangent_dist" : "");
+  }
+  fs << "flags" << flags;
+  fs << "camera_matrix" << cameraMatrix;
+  fs << "distortion_coefficients" << distCoeffs;
+  fs << "avg_reprojection_error" << totalAvgErr;
+  return true;
 }
 
 void trackLineFollower(const detectionOptions& options);
