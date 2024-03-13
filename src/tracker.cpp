d344846d932a6d18f1652138da8609109d5a5b1c
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-03-13 16:14:52 -0400

    Initial implementation of the camera calibration function

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -150,7 +150,111 @@ void tracker::trackLineFollower(const tracker::detectionOptions& options)
   }
 }
 
-void tracker::calibrateCamera()
+void tracker::calibrateCamera(const calibrationOptions& options, const calibrationOutput& output)
 {
+  bool showChessboardCorners {true};
+  int calibrationFlags {0};
+  cv::aruco::DetectorParameters detectorParams {cv::aruco::DetectorParameters()};
 
+  cv::VideoCapture inputVideo;
+  int waitTime;
+
+  if(options.inputFilePath != "none") {
+    inputVideo.open(options.inputFilePath);
+    std::cout << "Using input source file instead of camera stream" << std::endl;
+    waitTime = 0;
+  }
+  else {
+    inputVideo.open(options.camID);
+    waitTime = 10;
+  }
+
+  cv::aruco::CharucoBoard board(
+    cv::Size(options.squaresQuantityX, options.squaresQuantityY),
+    options.squareSideMeters,
+    options.markerSideMeters,
+    options.arucoDictionary
+  );
+
+  cv::aruco::CharucoParameters charucoParams;
+  cv::aruco::CharucoDetector detector(board, charucoParams, detectorParams);
+
+  std::vector<cv::Mat> allCharucoCorners;
+  std::vector<cv::Mat> allCharucoIds;
+
+  std::vector<std::vector<cv::Point2f>> allImagePoints;
+  std::vector<std::vector<cv::Point3f>> allObjectPoints;
+
+  std::vector<cv::Mat> allImages;
+  cv::Size imageSize;
+
+  while(inputVideo.grab()) {
+    cv::Mat image, imageCopy;
+    inputVideo.retrieve(image);
+
+    std::vector<int> markerIds;
+    std::vector<std::vector<cv::Point2f>> markerCorners, rejectedMarkers;
+    cv::Mat currentCharucoCorners;
+    cv::Mat currentCharucoIds;
+    std::vector<cv::Point3f> currentObjectPoints;
+    std::vector<cv::Point2f> currentImagePoints;
+
+    // Detect ChArUco board
+    detector.detectBoard(image, currentCharucoCorners, currentCharucoIds);
+
+    // Draw results
+    image.copyTo(imageCopy);
+    if(!markerIds.empty()) {
+      cv::aruco::drawDetectedMarkers(imageCopy, markerCorners);
+    }
+
+    if(currentCharucoCorners.total() > 3) {
+      cv::aruco::drawDetectedCornersCharuco(imageCopy, currentCharucoCorners, currentCharucoIds);
+    }
+
+    cv::putText(imageCopy, "Press 'c' to add current frame. 'ESC' to finish and calibrate",
+        cv::Point(10, 20), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(255, 0, 0), 2);
+
+    cv::imshow("out", imageCopy);
+
+    // Wait for key pressed
+    char key = (char)cv::waitKey(waitTime);
+
+    if(key == 27)
+      break;
+
+    if(key == 'c' && currentCharucoCorners.total() > 3) {
+      // Match image points
+      board.matchImagePoints(currentCharucoCorners, currentCharucoIds, currentObjectPoints, currentImagePoints);
+
+      if(currentImagePoints.empty() || currentObjectPoints.empty()) {
+        std::cout << "Point matching failed, try again." << std::endl;
+        continue;
+      }
+
+      std::cout << "Frame captured" << std::endl;
+
+      allCharucoCorners.push_back(currentCharucoCorners);
+      allCharucoIds.push_back(currentCharucoIds);
+      allImagePoints.push_back(currentImagePoints);
+      allObjectPoints.push_back(currentObjectPoints);
+      allImages.push_back(image);
+
+      imageSize = image.size();
+    }
+  }
+
+  if(allCharucoCorners.size() < 4) {
+    std::cerr << "Not enough corners for calibration" << std::endl;
+    return;
+  }
+
+  cv::Mat cameraMatrix, distCoeffs;
+
+  // Calibrate camera using ChArUco
+  double repError = cv::calibrateCamera(
+    allObjectPoints, allImagePoints, imageSize,
+    cameraMatrix, distCoeffs, cv::noArray(), cv::noArray(), cv::noArray(),
+    cv::noArray(), cv::noArray(), calibrationFlags
+  );
 }
