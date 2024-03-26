721676c4370d3399e20021ed52b7121cb0e52aea
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-03-26 00:04:17 -0400

    Updated main function with the new changes and additions

--- src/main.cpp
+++ src/main.cpp
@@ -23,21 +23,67 @@ int main(int argc, char *argv[]) {
   //   return 0;
   // }
 
-  if (!parser.has("config")) {
-    std::cerr << "A configuration file is required to run the program." << std::endl;
-    parser.printMessage();
-    return 1;
-  }
-
-  tracker::detectionOptions options {};
-  tracker::readConfigFile(parser.get<std::string>("config"), options);
+  bool hasConfigFile {parser.has("config")};
+  bool hasOutputDir {parser.has("output")};
+  bool calibrationMode {parser.get<bool>("calibration")};
 
   if(!parser.check()) {
     parser.printErrors();
     return 1;
   }
 
-  tracker::trackLineFollower(options);
+  if (!hasConfigFile && !calibrationMode) {
+    std::cerr << "A configuration file is required for tracking." << std::endl;
+    parser.printMessage();
+    return 1;
+  }
+  else if (!hasConfigFile && !hasOutputDir && calibrationMode) {
+    std::cerr << "At least an output directory is required for calibration.";
+    parser.printMessage();
+    return 1;
+  }
+
+  // TODO: Finish this.
+  tracker::detectionOptions detectionOptions {};
+  tracker::calibrationOptions calibrationOptions {};
+  tracker::calibrationOutput calibrationOutput {};
+
+  if (!calibrationMode) {
+    auto fileName {parser.get<std::string>("config")};
+    tracker::readConfigFile(fileName, detectionOptions);
+    tracker::trackLineFollower(detectionOptions);
+  }
+  else {
+    if (hasConfigFile) {
+      auto fileName {parser.get<std::string>("config")};
+      tracker::readConfigFile(fileName, detectionOptions);
+      tracker::readConfigFile(fileName, calibrationOptions);
+      tracker::calibrateCamera(calibrationOptions, calibrationOutput);
+
+      if (hasOutputDir) {
+        auto outputDir {parser.get<std::string>("output")};
+        auto fileNameStream {tracker::createConfigFileName(outputDir)};
+        tracker::writeConfigFile(fileNameStream.str(), detectionOptions, calibrationOptions, calibrationOutput);
+      }
+      else {
+        std::remove(fileName.c_str());
+        tracker::writeConfigFile(fileName, detectionOptions, calibrationOptions, calibrationOutput);
+      }
+
+      return 0;
+    }
+    else if (!hasConfigFile && hasOutputDir) {
+      std::cout << "Creating a new config file in the output directory..." << std::endl;
+      auto outputDir {parser.get<std::string>("output")};
+
+      auto fileNameStream {tracker::createConfigFileName(outputDir)};
+
+      tracker::writeConfigFile(fileNameStream.str(), detectionOptions, calibrationOptions, calibrationOutput);
+      std::cout << "Config file created: " << fileNameStream.str() << std::endl;
+
+      return 0;
+    }
+  }
 
   return 0;
 }
