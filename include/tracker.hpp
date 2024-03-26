5e4df58c55f7cd5b3cef374e017ffdd9f78f0b7d
Abhishekh <126507842+armgits@users.noreply.github.com>
2024-03-26 00:01:54 -0400

    Fully declared the method to write configuration files

--- include/tracker.hpp
+++ include/tracker.hpp
@@ -111,40 +111,11 @@ void readConfigFile(const std::string& filename, detectionOptions& options);
 void readConfigFile(const std::string& filename, calibrationOptions& options);
 
 // TODO: Finish implementing this method
-void writeConfigFile();
-
-// TODO: Delete this after using it as a reference for writing data a YAML file
-inline static bool saveCameraParams(const std::string &filename, cv::Size imageSize, float aspectRatio, int flags,
-                                    const cv::Mat &cameraMatrix, const cv::Mat &distCoeffs, double totalAvgErr) {
-  cv::FileStorage fs(filename, cv::FileStorage::WRITE);
-  if (!fs.isOpened())
-    return false;
-
-  time_t tt;
-  time(&tt);
-  struct tm *t2 = localtime(&tt);
-  char buf[1024];
-  strftime(buf, sizeof(buf) - 1, "%c", t2);
-
-  fs << "calibration_time" << buf;
-  fs << "image_width" << imageSize.width;
-  fs << "image_height" << imageSize.height;
-
-  if (flags & cv::CALIB_FIX_ASPECT_RATIO) fs << "aspectRatio" << aspectRatio;
-
-  if (flags != 0) {
-    sprintf(buf, "flags: %s%s%s%s",
-      flags & cv::CALIB_USE_INTRINSIC_GUESS ? "+use_intrinsic_guess" : "",
-      flags & cv::CALIB_FIX_ASPECT_RATIO ? "+fix_aspectRatio" : "",
-      flags & cv::CALIB_FIX_PRINCIPAL_POINT ? "+fix_principal_point" : "",
-      flags & cv::CALIB_ZERO_TANGENT_DIST ? "+zero_tangent_dist" : "");
-  }
-  fs << "flags" << flags;
-  fs << "camera_matrix" << cameraMatrix;
-  fs << "distortion_coefficients" << distCoeffs;
-  fs << "avg_reprojection_error" << totalAvgErr;
-  return true;
-}
+void writeConfigFile(
+  const std::string& filename,
+  const detectionOptions& detection_options,
+  const calibrationOptions& calibration_options,
+  const calibrationOutput& calibration_output);
 
 void trackLineFollower(const detectionOptions& options);
 void calibrateCamera(const calibrationOptions& options, const calibrationOutput& output);
