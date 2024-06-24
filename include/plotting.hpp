97e6e487fce936b1b4a63de11d17a34f19b80bd2
Abhishekh Reddy <126507842+armgits@users.noreply.github.com>
2024-06-23 22:13:06 -0400

    Initial plotting class and functions implementation

---
+++ include/plotting.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <opencv2/core/types.hpp>
+
+#include <matplot/matplot.h>
+// #include <matplot/backend/opengl.h>
+
+#include "options.hpp"
+#include "fileio.hpp"
+
+namespace plotting {
+
+class Plotter {
+
+public:
+  Plotter(const options::BoardMarkers& boardMarkersOptions);
+
+  void setReferenceTrack(const options::LineTrack& referenceTrackIn);
+  void setReferenceTrack(const options::RoundTrack& referenceTrackIn);
+
+  void savePosition(const cv::Point2d& positionIn);
+  void saveError(double errorIn, double timeIn);
+  void savePlots(const fileio::OutputPath& outputPathIn);
+
+private:
+  matplot::figure_handle _errorPlotFigure;
+  matplot::figure_handle _positionPlotFigure;
+
+  matplot::axes_handle _errorPlotAxesHandle;
+  matplot::axes_handle _positionPlotAxesHandle;
+
+  std::pair<std::vector<double>, std::vector<double>> _lineFollowerPositions;
+  std::pair<std::vector<double>, std::vector<double>> _trackingErrors;
+  std::pair<std::vector<double>, std::vector<double>> _referenceTrackPoints;
+
+};
+
+}
+
+// auto plotFigure {matplot::figure<matplot::backend::opengl>(true)};
+// auto errorAxesHandle {plotFigure->add_subplot(2, 1, 0)};
+// auto positionAxesHandle {plotFigure->add_subplot(2, 1, 1)};
+// errorAxesHandle->title("Error Plot");
+// positionAxesHandle->title("Position");
+// std::pair<std::vector<double>, std::vector<double>> lineFollowerPositions;
+// std::pair<std::vector<double>, std::vector<double>> trackingErrors;
---
+++ src/plotting.cpp
@@ -0,0 +1,86 @@
+#include "plotting.hpp"
+
+plotting::Plotter::Plotter(const options::BoardMarkers& boardMarkersOptions)
+: _errorPlotFigure {matplot::figure(true)},
+  _positionPlotFigure {matplot::figure(true)},
+  _errorPlotAxesHandle {_errorPlotFigure->current_axes()},
+  _positionPlotAxesHandle {_positionPlotFigure->current_axes()},
+  _lineFollowerPositions {{}, {}},
+  _trackingErrors {{}, {}},
+  _referenceTrackPoints {{}, {}}
+{
+  // TODO: Do some initial testing and find the maximum time needed to track poses in each run.
+  //       Use that to reserve those many points for _lineFollowerPositions and _trackErrors vectors.
+  _errorPlotFigure->size(1000, 500);
+  _positionPlotFigure->size(1000, 640);
+
+  _errorPlotAxesHandle->title("Error Plot");
+  _errorPlotAxesHandle->xlabel("Time (seconds)");
+  _errorPlotAxesHandle->ylabel("Error (meters)");
+  _errorPlotAxesHandle->axes_aspect_ratio(1 / 2); // Aspect ratio: Height / Width
+
+  _positionPlotAxesHandle->title("Line Follower Position");
+  _positionPlotAxesHandle->xlabel("X Position (meters)");
+  _positionPlotAxesHandle->ylabel("Y Position (meters)");
+
+  _positionPlotAxesHandle->xlim({0, boardMarkersOptions.markerSeperationMetersX});
+  _positionPlotAxesHandle->ylim({0, boardMarkersOptions.markerSeperationMetersY});
+  _positionPlotAxesHandle->axes_aspect_ratio(
+    boardMarkersOptions.markerSeperationMetersY / boardMarkersOptions.markerSeperationMetersX);
+}
+
+void plotting::Plotter::setReferenceTrack(const options::LineTrack& referenceTrackIn)
+{
+  _referenceTrackPoints.first.clear();
+  _referenceTrackPoints.second.clear();
+  _referenceTrackPoints.first.reserve(4);
+  _referenceTrackPoints.second.reserve(4);
+
+  _referenceTrackPoints = {
+    {referenceTrackIn.point1.x, referenceTrackIn.point2.x},
+    {referenceTrackIn.point1.y, referenceTrackIn.point2.y}};
+}
+
+void plotting::Plotter::setReferenceTrack(const options::RoundTrack& referenceTrackIn)
+{
+  _referenceTrackPoints.first.clear();
+  _referenceTrackPoints.second.clear();
+  _referenceTrackPoints.first.reserve(361);
+  _referenceTrackPoints.second.reserve(361);
+
+  for (double theta {0.0}; theta < 2 * M_PI; theta += M_PI / 180) {
+    _referenceTrackPoints.first.push_back(referenceTrackIn.semiMajorAxis() * std::cos(theta));
+    _referenceTrackPoints.second.push_back(referenceTrackIn.semiMinorAxis() * std::sin(theta));
+  }
+}
+
+void plotting::Plotter::savePosition(const cv::Point2d& positionIn)
+{
+  _lineFollowerPositions.first.push_back(positionIn.x);
+  _lineFollowerPositions.second.push_back(positionIn.y);
+}
+
+void plotting::Plotter::saveError(double errorIn, double timeIn)
+{
+  _trackingErrors.first.push_back(timeIn);
+  _trackingErrors.second.push_back(errorIn);
+}
+
+void plotting::Plotter::savePlots(const fileio::OutputPath& outputPathIn)
+{
+  std::stringstream errorPlotPath {
+    fileio::createPath(
+      outputPathIn.directoryPath.str(), "error-plot", outputPathIn.outputName, "jpg")};
+
+  _errorPlotAxesHandle->plot(_trackingErrors.first, _trackingErrors.second, "-r");
+  _errorPlotFigure->save(errorPlotPath.str());
+
+  std::stringstream positionPlotPath {
+    fileio::createPath(
+      outputPathIn.directoryPath.str(), "positions-plot", outputPathIn.outputName, "jpg")};
+
+  _positionPlotAxesHandle->plot(_referenceTrackPoints.first, _referenceTrackPoints.second, "b");
+  _positionPlotAxesHandle->hold(matplot::on);
+  _positionPlotAxesHandle->plot(_lineFollowerPositions.first, _lineFollowerPositions.second, "-o");
+  _positionPlotFigure->save(positionPlotPath.str());
+}
