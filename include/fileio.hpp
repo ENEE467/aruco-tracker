ab797d5ce6f9338d5da5c7bdf7db931a2bbaeb13
Abhishekh Reddy <126507842+armgits@users.noreply.github.com>
2024-06-23 21:29:15 -0400

    Updated config file read methods with the new structs

--- include/fileio.hpp
+++ include/fileio.hpp
@@ -6,10 +6,15 @@
 
 namespace fileio {
 
-void readConfigFile(const std::string& filename, options::MarkerDetection& options);
-void readConfigFile(const std::string& filename, options::LineFollowerMarker& options);
-void readConfigFile(const std::string& filename, options::BoardMarkers& options);
-void readConfigFile(const std::string& filename, options::Calibration& options);
+void readConfigFile(const std::string& filenameIn, options::MarkerDetection& optionsOut);
+void readConfigFile(const std::string& filenameIn, options::LineFollowerMarker& optionsOut);
+void readConfigFile(const std::string& filenameIn, options::BoardMarkers& optionsOut);
+void readConfigFile(const std::string& filenameIn, options::Track& optionsOut);
+void readConfigFile(const std::string& filenameIn, options::CalibrationBoard& optionsOut);
+void readConfigFile(const std::string& filenameIn, options::CameraIntrinsic& parametersOut);
+
+void readConfigFile(const std::string& filenameIn, options::Tracking& optionsOut);
+void readConfigFile(const std::string& filenameIn, options::Calibration& optionsOut);
 
 void writeConfigFile(
   const std::string& filename,
