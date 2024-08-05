c73832b160bdeab39f04742b83532d8dca4478ee
Abhishekh Reddy <126507842+armgits@users.noreply.github.com>
2024-08-05 19:40:23 -0400

    Cleaned up old, commented out code

--- src/application/application.cpp
+++ src/application/application.cpp
@@ -248,34 +248,6 @@ void InterfaceWindow::Update()
 
   // ImGui::ShowDemoWindow();
 
-  // Ask for configuration file.
-
-
-  // ImGui::Text("Provide a path to the input configuration file...");
-
-  // NFD_Init();
-
-  // nfdu8char_t *outPath;
-  // nfdu8filteritem_t filters[2] = { { "Configuration file", "yaml,YAML,yml" }};
-  // nfdopendialogu8args_t args = {0};
-  // args.filterList = filters;
-  // args.filterCount = 1;
-  // nfdresult_t result = NFD_OpenDialogU8_With(&outPath, &args);
-  // if (result == NFD_OKAY)
-  // {
-  //     std::cout << "Success!" << '\n';
-  //     std::cout << outPath << '\n';
-  //     NFD_FreePathU8(outPath);
-  // }
-  // else if (result == NFD_CANCEL)
-  // {
-  //     std::cout << "User pressed cancel." << '\n';
-  // }
-  // else
-  // {
-  //     std::cout << "Error: " << NFD_GetError() << '\n';
-  // }
-
   ImGui::End();
 }
 
